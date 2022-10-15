432287034c29dd2cc253c4532821aa0e4bccab97
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2022-10-15 18:54:32 +0700

    add hello.html

---
+++ script.sh
@@ -0,0 +1,10 @@
+git remote add origin https://github.com/chaerulmarwan20/git-remote.git
+git remote add origin git@github.com:chaerulmarwan20/git-remote.git
+git remote
+git remote -v
+git remote get-url origin
+git remote rm origin
+git push origin master
+git push origin master:develop
+git push origin --all
+git push --delete origin develop
