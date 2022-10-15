117fbcd3f1889cc22da84dba0a376f86b222b095
chaerulmarwan20 <chaerulmarwan20@gmail.com>
2022-10-15 19:27:20 +0700

    add version 1.0.0-SNAPSHOT

--- script.sh
+++ script.sh
@@ -8,3 +8,14 @@ git push origin master
 git push origin master:develop
 git push origin --all
 git push --delete origin develop
+git clone https://github.com/chaerulmarwan20/git-remote.git
+git clone git@github.com:chaerulmarwan20/git-remote.git
+git clone https://github.com/chaerulmarwan20/git-remote.git git-remote-clone
+git clone git@github.com:chaerulmarwan20/git-remote.git git-remote-clone
+git branch -r
+git branch -a
+git checkout -b feature/a origin/feature/a
+git fetch origin
+git fetch origin feature/a
+git diff feature/a..origin/feature/a
+git pull origin feature/a
